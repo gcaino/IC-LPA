3de1cef4ecaad1dddcb7fedffe706dd3ccde4a63
gcaino <german.caino@hotmail.com>
2017-11-23 14:46:48 -0300

    Modificacion .gitignore para incluir bin SFML

---
+++ IC-LPA-CodeBlocks/scripts/config.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set (PROJECT_DIR)=%1
+set (TARGET_OUTPUT_DIR)=%2
+
+copy "%(PROJECT_DIR)%libs\TDM-GCC-32\libstdc++-6.dll" "%(PROJECT_DIR)%%(TARGET_OUTPUT_DIR)%"
+copy "%(PROJECT_DIR)%libs\SFML-2.4.2-tdm\bin\sfml-graphics-d-2.dll" "%(PROJECT_DIR)%%(TARGET_OUTPUT_DIR)%"
+copy "%(PROJECT_DIR)%libs\SFML-2.4.2-tdm\bin\sfml-window-d-2.dll" "%(PROJECT_DIR)%%(TARGET_OUTPUT_DIR)%"
+copy "%(PROJECT_DIR)%libs\SFML-2.4.2-tdm\bin\sfml-system-d-2.dll" "%(PROJECT_DIR)%%(TARGET_OUTPUT_DIR)%"
---
+++ IC-LPA-CodeBlocks/scripts/test.bat
@@ -0,0 +1,5 @@
+set arg1=%1
+set arg2=%2
+
+@echo %arg1% > test-log.txt
+@echo %arg2% >> test-log.txt
