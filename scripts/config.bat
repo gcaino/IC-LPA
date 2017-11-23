1e26b52c7cbfe24e48530aad2ffc58fb1a6df990
gcaino <german.caino@hotmail.com>
2017-11-23 03:23:11 -0300

    Organizacion de proyecto en CodeBlocks

--- scripts/config.bat
+++
@@ -1,8 +0,0 @@
-cd C:\Program Files\ZinjaI\mingw32-gcc5\bin
-copy libstdc*.* C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\SFMLTest\Debug
-
-copy C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\Libs\SFML-2.4.2\bin\sfml-graphics-d-2.dll C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\SFMLTest\Debug
-
-copy C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\Libs\SFML-2.4.2\bin\sfml-window-d-2.dll C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\SFMLTest\Debug
-
-copy C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\Libs\SFML-2.4.2\bin\sfml-system-d-2.dll C:\Users\Ger\Desktop\German\ImageCampus\LPA\Clase_21\SFMLTest\Debug
