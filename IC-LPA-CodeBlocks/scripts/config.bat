ea3f09ce8d9009779eb3718cfe3bf9082de5d995
gcaino <german.caino@hotmail.com>
2017-11-25 03:51:23 -0300

    Modificado post evento en CB para ejecutar config.bat

--- IC-LPA-CodeBlocks/scripts/config.bat
+++ IC-LPA-CodeBlocks/scripts/config.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set (PROJECT_DIR)=%1
-set (TARGET_OUTPUT_DIR)=%2
+set (PROJECT_DIR)=%~1
+set (TARGET_OUTPUT_DIR)=%~2
 
 copy "%(PROJECT_DIR)%libs\TDM-GCC-32\libstdc++-6.dll" "%(PROJECT_DIR)%%(TARGET_OUTPUT_DIR)%"
 copy "%(PROJECT_DIR)%libs\SFML-2.4.2-tdm\bin\sfml-graphics-d-2.dll" "%(PROJECT_DIR)%%(TARGET_OUTPUT_DIR)%"
