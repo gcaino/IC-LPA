b2612e438e29fc5f58246070f553eb5edfe8024d
gcaino <german.caino@hotmail.com>
2017-12-19 20:15:04 -0300

    [ADD] música y librerías para sonido

--- IC-LPA-VS2017/scripts/config.bat
+++ IC-LPA-VS2017/scripts/config.bat
@@ -8,6 +8,7 @@ IF "%(CONFIGURATION)%"=="DEBUG" (
 Xcopy "%(PROJECT_DIR)%libs\SFML-2.4.2-vc14-32\SFML-2.4.2\bin\sfml-graphics-d-2.dll" "%(TARGET_OUTPUT_DIR)%" /E /D 
 Xcopy "%(PROJECT_DIR)%libs\SFML-2.4.2-vc14-32\SFML-2.4.2\bin\sfml-window-d-2.dll" "%(TARGET_OUTPUT_DIR)%" /E /D 
 Xcopy "%(PROJECT_DIR)%libs\SFML-2.4.2-vc14-32\SFML-2.4.2\bin\sfml-system-d-2.dll" "%(TARGET_OUTPUT_DIR)%" /E /D 
+Xcopy "%(PROJECT_DIR)%libs\SFML-2.4.2-vc14-32\SFML-2.4.2\bin\sfml-audio-d-2.dll" "%(TARGET_OUTPUT_DIR)%" /E /D 
 
 Xcopy "%(PROJECT_DIR)%libs\VLD\bin\Win32\dbghelp.dll" "%(TARGET_OUTPUT_DIR)%" /E /D 
 Xcopy "%(PROJECT_DIR)%libs\VLD\bin\Win32\Microsoft.DTfW.DHL.manifest" "%(TARGET_OUTPUT_DIR)%" /E /D 
@@ -15,3 +16,4 @@ Xcopy "%(PROJECT_DIR)%libs\VLD\bin\Win32\vld_x86.dll" "%(TARGET_OUTPUT_DIR)%" /E
 )
 
 Xcopy /E /I /Y /D "%(PROJECT_DIR)%assets" "%(TARGET_OUTPUT_DIR)%assets"
+Xcopy "%(PROJECT_DIR)%libs\SFML-2.4.2-vc14-32\SFML-2.4.2\bin\openal32.dll" "%(TARGET_OUTPUT_DIR)%" /E /D
